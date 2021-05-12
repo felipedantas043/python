c523c1cdfbe328783acf5ddf18bde75464ba528c
Felipe Dantas de Araújo <72935710+felipedantas043@users.noreply.github.com>
2021-05-12 12:43:32 -0500

    Create ex027.py

---
+++ exercícios/ex027/ex027.py
@@ -0,0 +1,5 @@
+n = str(input('Digite seu nome completo: ')).lower().strip()
+nome = n.split()
+print('seu nome é {}'.format(nome))
+print('primeiro nome {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))

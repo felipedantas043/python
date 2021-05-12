1c26d5adb9660079f670a48712a2f101d7b1e081
Felipe Dantas de Araújo <72935710+felipedantas043@users.noreply.github.com>
2021-05-12 11:15:50 -0500

    create files

---
+++ exercícios/ex024/ex024.py
@@ -0,0 +1,3 @@
+cid = input('Em qual cidade você nasceu?').strip().lower()
+print(cid[:5] == "santo")
+print(cid)
---
+++ exercícios/ex025/ex025.py
@@ -0,0 +1,2 @@
+nome = input('digite seu nome completo: ').strip()
+print('Seu nome tem silva? {}'.format('silva' in nome.lower()))
